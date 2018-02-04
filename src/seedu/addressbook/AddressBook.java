f347ee11bd9e0a6c044c71100eea6afb34b6df5a
sarahgoh97 <sarahgohshining97@yahoo.com.sg>
2018-02-04 16:18:51 +0800

    Refactoring
    Refactoring while loop into processUserCommand() and changing VERSION from "AddessBook" to "AddressBook"

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -44,7 +44,7 @@ public class AddressBook {
     /**
      * Version info of the program.
      */
-    private static final String VERSION = "AddessBook Level 1 - Version 1.0";
+    private static final String VERSION = "AddressBook Level 1 - Version 1.0";
 
     /**
      * A decorative prefix added to the beginning of lines printed by AddressBook
@@ -210,6 +210,11 @@ public class AddressBook {
         showWelcomeMessage();
         processProgramArgs(args);
         loadDataFromStorage();
+        processUserCommand();
+        return;
+    }
+
+    private static void processUserCommand() {
         while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
